70be39da037b02b5432ec314a28d6e7f750dd4b1
Raffael Schneider <raffael.schneider@b-nova.com>
2021-07-21 16:01:51 +0200

    - fixed matchesPattern unit test

--- pkg/filter/filter_test.go
+++ pkg/filter/filter_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 )
 
-func TestDoSomething(t *testing.T) {
+func TestMatchesPattern(t *testing.T) {
 	patterns := []string{"/home/content/*"}
-	got := matchesPattern(patterns, "/home/content/blog.html")
+	got := matchesPattern(patterns, "/home/content/blog.html", "")
 
 	assert.Equal(t, got, true, "They should be equal!")
 }
