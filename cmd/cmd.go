d5f9287cd54945e0f7f6379d1ff92dadf6f84aad
Raffael Schneider <raffael.schneider@b-nova.com>
2021-07-21 15:58:09 +0200

    - added config file and config file parsing

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,10 +1,15 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
 )
 
 var (
+	cfgFile string
+
 	rootCmd = &cobra.Command{
 		Use:           "fisicus",
 		Short:         "fisicus – command-line tool to manage {PLEASE DESCRIBE}",
@@ -20,5 +25,27 @@ func Execute() error {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
+
 	rootCmd.AddCommand(serveCmd)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home)
+		viper.AddConfigPath(".")
+		viper.SetConfigType("yaml")
+		viper.SetConfigFile(".fisicus")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Config file used for fisicus: ", viper.ConfigFileUsed())
+	}
 }
