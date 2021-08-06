14fe7867c0f1b009e9cce43bb1d7cf7ac625dbfe
Raffael Schneider <raffael.schneider@b-nova.com>
2021-08-06 09:50:07 +0200

    - implemented url reading from fisicus - adjusted tokenizing - general refactoring

--- fisicus.sh
+++ fisicus.sh
@@ -5,5 +5,6 @@ echo "$(<LICENSE)"
 # Configure your fisicus app below:
 SITEMAP=https://b-nova.com/sitemaps/sitemap.xml
 FILTER=/home/content/*
+PORT=8000
 
-./bin/fisicus serve --sitemap=$SITEMAP --filter=$FILTER
+./bin/fisicus serve --sitemap=$SITEMAP --filter=$FILTER --port=$PORT
